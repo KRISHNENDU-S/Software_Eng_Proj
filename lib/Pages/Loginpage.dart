5557363cce941f6bda5464506282d1b3e85d9d04
Rithish <rithish675@gmail.com>
2024-04-07 17:45:49 +0530

    I have included loginpage

---
+++ lib/Pages/Loginpage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class Loginpage extends StatefulWidget{
+  const Loginpage({super.key});
+  @override
+  State<Loginpage> createState() => _Loginpagestate();
+}
+
+
+class _Loginpagestate extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Icon(Icons.abc_rounded,size: 80,)
+
+        ],
+      ),
+    );
+  }
+}
